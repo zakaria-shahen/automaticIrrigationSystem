b1dba89dbccf04fa4f55701753a6390f2f4ae337
zakaria-shahen <zakariay.shahen@gmail.com>
2022-12-26 18:20:49 +0200

    init

---
+++ src/test/java/com/company/automaticirrigationsystem/AutomaticIrrigationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.automaticirrigationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticIrrigationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
