f59ac0a5f72427593c0830aea33a035bbca3bd12
zakaria-shahen <zakariay.shahen@gmail.com>
2022-12-31 14:17:06 +0200

    At startup, Publishing sendIrrigationRequestEvent for Stored Plots of land If Queue is Empty

--- src/main/java/com/company/automaticirrigationsystem/event/SendIrrigationRequestEvent.java
+++ src/main/java/com/company/automaticirrigationsystem/event/SendIrrigationRequestEvent.java
@@ -6,15 +6,22 @@ import com.company.automaticirrigationsystem.service.IrrigationLogService;
 import com.company.automaticirrigationsystem.service.PlotService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.MessageProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.support.GenericMessage;
 
 import java.util.Map;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.function.Consumer;
+
+import static org.springframework.amqp.rabbit.core.RabbitAdmin.QUEUE_MESSAGE_COUNT;
 
 @Configuration
 @RequiredArgsConstructor
@@ -22,10 +29,13 @@ import java.util.function.Consumer;
 public class SendIrrigationRequestEvent {
 
     public static final String OUTPUT_EVENT_NAME = "sendIrrigationRequestEvent-out-0";
+    @Value("${app.events.sendIrrigationRequest.queue_name}")
+    private String queueName;
 
     private final IrrigationLogService irrigationLogService;
     private final PlotService plotService;
     private final StreamBridge streamBridge;
+    private final AmqpAdmin amqpAdmin;
 
 
     @Bean
@@ -64,5 +74,18 @@ public class SendIrrigationRequestEvent {
         }
     }
 
+    @EventListener(ApplicationStartedEvent.class)
+    public void publisherStoredPlotsIfQueueEmpty()  {
+
+        Properties properties = amqpAdmin.getQueueProperties(queueName);
+        if (Objects.isNull(properties) || properties.get(QUEUE_MESSAGE_COUNT).equals(0)) {
+            log.debug("empty or not found sendIrrigationRequestEvent queue");
+            log.debug("Publishing sendIrrigationRequestEvent for stored plots if they exists");
+
+            plotService.findAll()
+                    .forEach(this::publisher);
+        }
+
+    }
 
 }
